641cd4fbf7d2ea6c96031f4044a3623902782c32
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-09-14 04:52:07 +0000

    main

--- run_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from src.homework.b_in_proc_out.output.py import multiply_numbers
-
-class Test_Config(unittest.TestCase):
-
-    def test_multiply_numbers(self):
-        self.assertEqual(25, multiply_numbers(5, 5))
-        self.assertEqual(49, multiply_numbers(7, 7))
