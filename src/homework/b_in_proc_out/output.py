5b6fc80ff5b4d101fb22c7fd56344c19cb2a491d
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-09-14 03:07:45 +0000

    multiply numbers

--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,2 +1,8 @@
-def get_number(num):
-    return num
+def hello_world(name):
+    print("Hello, " + name + "!")
+    
+def echo_value(value):
+    return value
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
--- tests/examples/a_example/tests_devprocess.py
+++ tests/examples/a_example/tests_devprocess.py
@@ -1,9 +1,11 @@
 import unittest
 
-from src.examples.a_example.devprocess import add_numbers
+from src.examples.a_example.devprocess import multiply_umbers
 
 class Test_Config(unittest.TestCase):
 
     def test_add_numbers_2(self):
-        self.assertEqual(2, add_numbers(1, 1))
+        self.assertEqual("Hello world!", echo_value("Hello world!"))
+        self.assertEqual("5", echo_value("5"))
+        
 
