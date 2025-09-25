ee1111e7c85ae9b8091e2f96c21d32d789ce3020
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-09-25 01:43:28 +0000

    unit testing

--- tests/homework/b_in_proc_out/tests_in_proc_out.py
+++ tests/homework/b_in_proc_out/tests_in_proc_out.py
@@ -4,15 +4,17 @@ import unittest
 The file at /src/homework/b_in_proc_out/output has 
 the function get_number.
 '''
-from src.homework.b_in_proc_out.output import get_number
+from src.homework.b_in_proc_out.output import get_sales_tax, get_tip_ammount
 
 class Test_Config(unittest.TestCase):
 
-    def test_get_number_1(self):
+    def test_get_sales_tax(self):
         #test that the function get_number returns 1
-        self.assertEqual(1, get_number(1))
+        self.assertEqual(6.75, get_sales_tax(100))
+        self.assertEqual(13.5, get_sales_tax(200))
     
-    def test_get_number_2(self):
+    def test_get_tip_ammount(self):
         #test that the function get_number returns 2
-        self.assertEqual(2, get_number(2))
+        self.assertEqual(20, get_tip_ammount(100, .2))
+        self.assertEqual(40, get_tip_ammount(200, .2))
 
