c199cb68b0e29112da6202e2ecd615fcdc532470
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-09-26 04:02:43 +0000

    numerical functions updated

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -4,13 +4,13 @@ def numerical_grade_to_letter_grade(numerical_grade):
 
     letter_grade = ""
 
-    if (numerical_grade >= 90 and numerical_grade <=100)
+    if (numerical_grade >= 90 and numerical_grade <=100):
         letter_grade = "A"
-    elif(numerical_grade >= 80 and numerical_grade <=89)
+    elif(numerical_grade >= 80 and numerical_grade <=89):
         letter_grade = "B"
-    elif (numerical_grade >= 70 and numerical_grade <=79)
+    elif (numerical_grade >= 70 and numerical_grade <=79):
         letter_grade = "C"
-    elif (numerical_grade >= 60 and numerical_grade <=69)
+    elif (numerical_grade >= 60 and numerical_grade <=69):
         letter_grade = "D"
-    else numerical_grade >=0:
+    else:
         letter_grade = "F"
