97b3ba7fcd805c1b04fb27840466e375ccb1b431
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-09-26 02:13:59 +0000

    numerical grade conversion function

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,14 @@
-#
+#convert from number to letter
+
+def numerical_grade_to_letter(numerical_grade):
+
+    if numerical_grade >= 90:
+        return "A"
+    elif numerical_grade >= 80:
+        return "B"
+    elif numerical_grade >= 70:
+        return "C"
+    elif numerical_grade >= 60:
+        return "D"
+    else:
+        return "F"
