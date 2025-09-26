4dabbea505f9ee45536ff1adeb28793428b044dd
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-09-26 04:03:07 +0000

    main updated

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,16 @@
-#
+from decisions import numerical_grade_to_letter_grade
+
+def main():
+    print("       MAIN MENU")
+    print("1 - Letter grade using if")
+    print("2 - Letter grade using switch")
+    print("3 - Exit")
+
+    selection = input("Enter selection here: ")
+    if selection == "1":
+        numerical_grade = input("Enter numerical grade: ")
+        numerical_grade_to_letter_grade(numerical_grade)
+
+
+
+main()
