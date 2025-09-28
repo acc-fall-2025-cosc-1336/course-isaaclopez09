08ea2cf1c62feb7ebf01db0a92e318a9d60d9a85
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-09-28 02:51:38 +0000

    updated unittests

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,10 +1,11 @@
 import unittest
 
-from src.homework.c_decisions.decisions import numerical_grade_to_letter_grade
+from src.homework.c_decisions.decisions import get_letter_grade
 
-def test_numerical_grade_to_letter_grade(self):
-    self.assertEqual(numerical_grade_to_letter_grade(95), "A")
-    self.assertEqual(numerical_grade_to_letter_grade(85), "b")
-    self.assertEqual(numerical_grade_to_letter_grade(75), "C")
-    self.assertEqual(numerical_grade_to_letter_grade(65), "D")
-    self.assertEqual(numerical_grade_to_letter_grade(50), "F")
+class TestGradeConversion(unittest.TestCase):
+    def test_get_letter_grade(self):
+        self.assertEqual(get_letter_grade(95), 'A')
+        self.assertEqual(get_letter_grade(85), "B")
+        self.assertEqual(get_letter_grade(75), "C")
+        self.assertEqual(get_letter_grade(65), "D")
+        self.assertEqual(get_letter_grade(50), "F")
