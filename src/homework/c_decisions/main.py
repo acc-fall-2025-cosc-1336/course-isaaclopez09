c255a6b6835d7f960e981b5cf841056b8f0df0b8
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-09-28 02:52:34 +0000

    updated main function

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,4 +1,4 @@
-from decisions import numerical_grade_to_letter_grade
+from decisions import get_letter_grade
 
 def main():
     print("       MAIN MENU")
@@ -8,9 +8,27 @@ def main():
 
     selection = input("Enter selection here: ")
     if selection == "1":
-        numerical_grade = input("Enter numerical grade: ")
-        numerical_grade_to_letter_grade(numerical_grade)
+        numerical_grade = int(input("Enter a number between 0 and 100: "))
+        if 0 <= numerical_grade <= 100:
 
+            grade = get_letter_grade(numerical_grade)
+            print(f"The letter grade using if is: {grade}")
+        else:
+            print("Numerical grade must be between 0 and 100")
 
+    elif selection == "2":
+        numerical_grade = int(input("Enter a number between 0 and 100: "))
+        if 0 <= numerical_grade <= 100:
+
+            grade = get_letter_grade(numerical_grade)
+            print(f"The letter grade using switch is: {grade}")
+        else:
+            print("Numerical grade must be between 0 and 100")
+
+    elif selection == '3':
+        print("Exiting the program.")
+
+    else:
+        print("invalid entry, please select 1,2,or 3")
 
 main()
