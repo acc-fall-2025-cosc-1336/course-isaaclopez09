20b915115f9cf99e43b6ef0537dfc8a86d5dea5f
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-10-03 03:51:08 +0000

    continued main menu

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,7 +1,16 @@
-from src.homework.d_repetition.repetition import get_factorial, sum_odd_numbers
+from repetition import get_factorial, sum_odd_numbers
 
 
 print("MAIN MENU")
 print("1 - Factorial")
 print("2 - Sum odd numbers")
-print("3- exit")
+print("3 - exit")
+selection = input("Enter selection here: ")
+if selection == "1":
+        input_number = int(input("Enter any positive number: "))
+        if 0 <= input_number:
+
+            factorial = get_factorial(input_number)
+            print(f"The factorial of {input_number} is: {factorial}")
+        else:
+            print("Number entered must be positive")
