18636806528f3ac6641a8e33426e083d9e0bf232
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-10-03 03:28:51 +0000

    updated unittests

--- tests/homework/d_repetition/tests_repetition.py
+++ tests/homework/d_repetition/tests_repetition.py
@@ -2,4 +2,12 @@ import unittest
 
 from src.homework.d_repetition.repetition import get_factorial, sum_odd_numbers
 
-class Test_Config(unittest.TestCase):
+class Test_get_factorial(unittest.TestCase):
+    def test_get_factorial(self):
+        self.assertEqual(get_factorial(0), 1)
+        self.assertEqual(get_factorial(3), 6)
+        self.assertEqual(get_factorial(5), 120)
+        self.assertEqual(get_factorial(-1), "Factorial is not defined for negative numbers.")
+
+
+        
