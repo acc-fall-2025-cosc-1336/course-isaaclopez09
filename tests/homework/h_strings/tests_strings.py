acd0e007ba8b3ef417aa84a7f9f83561b6d566da
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-11-18 00:54:58 +0000

    unit test cases for strings

--- tests/homework/h_strings/tests_strings.py
+++ tests/homework/h_strings/tests_strings.py
@@ -1,1 +1,16 @@
-#
+import unittest
+
+from src.homework.h_strings.strings import get_hamming_distance, get_dna_complement
+
+
+class Test_get_hamming_distance(unittest.TestCase):
+    def test_get_hamming_distance(self):
+        dna1 = "GAGCCTACTAACGGGAT"
+        dna2 = "CATCGTAATGACGGCCT"
+        self.assertEqual(get_hamming_distance(dna1, dna2), 7)
+
+class Test_get_dna_complement(unittest.TestCase):
+    def test_get_dna_complement(self):
+        dna = "AAAACCCGGT"
+        self.assertEqual(get_dna_complement(dna), "ACCGGGTTTT")
+
