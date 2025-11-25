02f8380343f07fbcfbf18bd6257db0d672356c20
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-11-25 06:47:24 +0000

    unit tests for lists

--- tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,1 +1,13 @@
-#
+import unittest
+
+from src.homework.g_lists_and_tuples.lists import get_lowest_list_value, get_highest_list_value
+
+class TestListFunctions(unittest.TestCase):
+
+    def test_get_lowest_list_value(self):
+        values = [8, 10, 1, 50, 20]
+        self.assertEqual(get_lowest_list_value(values), 1)
+
+    def test_get_highest_list_value(self):
+        values = [8, 10, 1, 50, 20]
+        self.assertEqual(get_highest_list_value(values), 50)
