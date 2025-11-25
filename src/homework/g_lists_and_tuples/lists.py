a8310b7d696007e46a697d55db1a3f98b6b0c173
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-11-25 06:53:33 +0000

    value return for lists

--- src/homework/g_lists_and_tuples/lists.py
+++ src/homework/g_lists_and_tuples/lists.py
@@ -1,1 +1,13 @@
-#
+def get_lowest_list_value(values):
+    lowest = values[0]
+    for v in values:
+        if v < lowest:
+            lowest = v
+    return lowest
+
+def get_highest_list_value(values):
+    highest = values[0]
+    for v in values:
+        if v > highest:
+            highest = v
+    return highest
