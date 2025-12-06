b7df64cad85bfb5989ab5ffbfeb329cd5dd6d131
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-12-06 00:27:38 +0000

    run unittests works now

--- run_tests.py
+++ run_tests.py
@@ -4,10 +4,7 @@ the file in /tests/homework/d_repetitions
 has the test functions
 '''
 
-from tests.homework.d_repetition.tests_repetition import get_factorial, sum_odd_numbers
+from tests.homework.d_repetition import tests_repetition
 
-suite = unittest.TestLoader().loadTestsFromModule(get_factorial)
-unittest.TextTestRunner(verbosity=2).run(suite)
-
-suite = unittest.TestLoader().loadTestsFromModule(sum_odd_numbers)
+suite = unittest.TestLoader().loadTestsFromModule(tests_repetition)
 unittest.TextTestRunner(verbosity=2).run(suite)
